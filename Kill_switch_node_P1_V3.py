0f54dfdbaf515c301e4b69e446ab0c4030b13fde
Peter Atrazhev <atrazhev@ualberta.ca>
2017-08-21 13:15:58 -0600

    Working build
    This should work. I just put all the effects into the callback function and then used the rospy.spin method.

---
+++ Kill_switch_node_P1_V3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+#import the dependencies
+import roslib
+import rospy
+
+#import the kobuki messages
+from kobuki_msgs.msg import ButtonEvent
+from kobuki_msgs.msg import MotorPower
+from kobuki_msgs.msg import Led
+
+class Kill_switchP1V3:
+
+    def __init__(self):
+        #constructor
+        rospy.init_node('Kill_switchP1V3')
+        self.button_sub = rospy.Subscriber('mobile_base/events/button',
+        ButtonCallback,self.ButtonEventCallback,queue_size = 1)
+
+        self.led_pub = rospy.Publisher('mobile_base/commands/led2',Led)
+        self.motor_pub = rospy.Publisher('mobile_base/commands/motor_power',
+        MotorPower)
+        self.state_count = 0
+        self.led_pub.publish(1)
+        self.motor_pub.publish(1)
+
+    def ButtonEventCallback(self,data):
+        if ( data.state == ButtonEvent.RELEASED ):
+                self.state = "released"
+        else:
+                self.state = "pressed"
+                #update the state_count variable
+                #execute the code here
+                if self.state_count == 0:
+                    #Red LED and motor off
+                    self.led_pub.publish(3)
+                    self.motor_pub.publish(0)
+                    self.state_count = 1
+                else:
+                    #Green LED and Motor on
+                    self.led_pub.publish(1)
+                    self.motor_pub.publish(1)
+                    self.state_count = 0
+
+def main():
+    KsP1V3 = Kill_switchP1V3()
+    rospy.spin()
